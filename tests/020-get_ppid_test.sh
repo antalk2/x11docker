ebccc505de4acfa9c79b8786f59c07daefa75f73
Antal K <antalk2@gmail.com>
2026-06-25 22:46:24 +0200

    xget_ppid -> get_ppid

--- tests/020-get_ppid_test.sh
+++ tests/020-get_ppid_test.sh
@@ -17,17 +17,17 @@ test_get_ppid() {
     #
     #
     local x expected
-    x="$(xget_ppid 1)"
+    x="$(get_ppid 1)"
     expected="0"
     assert_same "$expected" "$x"  "get_ppid 1"
     #
-    x="$(xget_ppid noshuchpid)"
+    x="$(get_ppid noshuchpid)"
     expected=""
     assert_same "$expected" "$x"  "get_ppid noshuchpid"
     #
-    x="$(xget_ppid)"
+    x="$(get_ppid)"
     expected=""
     assert_same "$expected" "$x" "missing-arg"
     #
-    assert_exec "xget_ppid" --exit 1 --stdout ""
+    assert_exec "get_ppid" --exit 1 --stdout ""
 }
