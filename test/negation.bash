6e47ac852f28f7729eba612f6c3271c83c5a575e
Antal K <antalk2@gmail.com>
2026-06-30 10:48:04 +0200

    negating pipe exitCode

--- test/negation.bash
+++ test/negation.bash
@@ -26,3 +26,14 @@ ret(){
 !     ret 13   ; echo $? # 0
 ! !   ret 21   ; echo $? # 21
 ! ( ! ret 11 ) ; echo $? # 1
+
+# Result: `! ! cmd` keeps exitCode of cmd
+
+echo "Testing: ! a | b"
+
+! true  | false ; echo $?  # 0
+! true  | true  ; echo $?  # 1
+! false | false ; echo $?  # 0
+! false | true  ; echo $?  # 1
+
+# Result: ! negates exitCode of b
