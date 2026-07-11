42c606d4600461e3e298eff508e2cc2c827ff4c1
Antal K <antalk2@gmail.com>
2026-07-11 23:30:15 +0200

    INTERNAL to HELPER

--- zz-unused/test/append_ops_to.sh
+++ zz-unused/test/append_ops_to.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
+NEWLINE=$'\n'
+create_xcommand_HELPER_append_ops_to() {
+    declare -n VAR_IO="$1"
+    shift
+    #
+    local op
+    local SEP=" \\${NEWLINE}  "
+    for op in "${@}" ; do
+        VAR_IO+="${SEP}${op}"
+    done
+}
+
 f() {
-    local NEWLINE=$'\n'
-    create_xcommand_INTERNAL_append_ops_to() {
-        declare -n VAR_IO="$1"
-        shift
-        #
-        local op
-        local SEP=" \\${NEWLINE}  "
-        for op in "${@}" ; do
-            VAR_IO+="${SEP}${op}"
-        done
-    }
-    local append_ops_to="create_xcommand_INTERNAL_append_ops_to"
+    local append_ops_to="create_xcommand_HELPER_append_ops_to"
     $append_ops_to Xserveroptions '-shmem' '-shpix'
     #
     # Can also add multiline op-collection.
